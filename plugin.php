565714ae141dd7788f2b86d5a4bf4565984ef8ef
Cloudbop <boppincolin@gmail.com>
2024-02-26 23:40:13 +0000

    +block+specification

--- plugin.php
+++ plugin.php
@@ -1,16 +1,16 @@
 <?php
 /**
- * Plugin Name:       Boilerplateblox
- * Description:       Example block scaffolded with Create Block tool.
+ * Plugin Name:        Team Members
+ * Description:       A team members grid.
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
  * Author:            The WordPress Contributors
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       boilerplateblox
+ * Text Domain:       team-members
  *
- * @package           create-block
+ * @package           blocks-course
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
