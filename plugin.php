5253c192ddb31e26bd09fbe9073556ee2e9014a0
Cloudbop <boppincolin@gmail.com>
2024-02-29 00:44:19 +0000

    +bp->refactor complete

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name:        Team Members
+ * Plugin Name:       Team Members
  * Description:       A team members grid.
  * Requires at least: 6.1
  * Requires PHP:      7.0
@@ -24,7 +24,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
-function boilerplateblox_boilerplateblox_block_init() {
+function blocks_course_team_members_block_init() {
 	register_block_type( __DIR__ . '/build' );
 }
-add_action( 'init', 'boilerplateblox_boilerplateblox_block_init' );
+add_action( 'init', 'blocks_course_team_members_block_init' );
