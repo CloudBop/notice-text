af086c06e34dca6217dd1a5fb5815ba61402739a
Cloudbop <boppincolin@gmail.com>
2024-03-03 22:15:01 +0000

    +updated meta

--- plugin.php
+++ plugin.php
@@ -1,16 +1,16 @@
 <?php
 /**
- * Plugin Name:       Blocks Course Team Members
- * Description:       A team members grid.
+ * Plugin Name:       Notice Text
+ * Description:       A text notice
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
  * Author:            The WordPress Contributors
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       team-members
+ * Text Domain:       notice-text
  *
- * @package           blocks-course
+ * @package           notice-text
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -24,7 +24,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
-function blocks_course_team_members_block_init() {
+function blox_notice_text_block_init() {
 	register_block_type( __DIR__  );
 }
-add_action( 'init', 'blocks_course_team_members_block_init' );
+add_action( 'init', 'blox_notice_text_block_init' );
